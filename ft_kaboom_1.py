731891bf43d266245384d112b566b230fba16fef
Rafael Sánchez-Piñol <146205862+rafaspy@users.noreply.github.com>
2026-09-18 14:30:30 +0200

    Add files via upload

--- ft_kaboom_1.py
+++ ft_kaboom_1.py
@@ -3,3 +3,4 @@ print("Access to alchemy/grimoire/dark_spellbook.py directly")
 print("Test import now - THIS WILL RAISE AN UNCAUGHT EXCEPTION")
 
 from alchemy.grimoire.dark_spellbook import dark_spell_record
+dark_spell_record('Fantasy', 'Earth, wind and fire')
